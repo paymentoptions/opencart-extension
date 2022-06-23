66ddd6a6e6d86e148229e5bb3dcfc3b7440684c8
PradeepGeo <108048932+PradeepGeo@users.noreply.github.com>
2022-06-23 15:28:30 +0530

    Initial commit

---
+++ upload/admin/language/en-gb/extension/payment/dasgatewayqr.php
@@ -0,0 +1,56 @@
+<?php
+// Heading
+$_['heading_title']				 = 'SME Dasgateway';
+
+// Text
+$_['text_extension']				 = 'Extensions';
+$_['text_success']				 = 'Success: You have modified Dasgateway payment details!';
+$_['text_edit']                      		 = 'Edit SME Dasgateway Payment Module';
+$_['text_notification_url']			 = 'Notification URL';
+$_['text_order_success_status']			 = 'Payment Success Status';
+$_['text_order_declined_status']		 = 'Payment Declined Status';
+$_['text_enabled']				 = 'Enabled';
+$_['text_live']	        	           	 = 'Live';
+$_['text_test']	    	       	           	 = 'Test';
+$_['text_live_publishable_key']			 = 'Live Publishable Key';
+$_['text_live_secret_key']			 = 'Live Secret Key';
+$_['text_test_publishable_key']			 = 'Test Publishable Key';
+$_['text_test_secret_key']			 = 'Test Secret Key';
+$_['text_merchant_id']	         		 = 'Merchant Id';
+$_['text_dasgatewayqr']				 = '<img src="view/image/payment/paymentoption.png" alt="Payment Option" title="SME Dasgateway" style="border: 1px solid #EEEEEE; width:100px;" />';
+$_['text_payment_info']				 = 'Payment information';
+
+
+// Entry
+$_['entry_live_publishable_key']		 = 'Live Publishable Key';
+$_['entry_live_secret_key']			 = 'Live Secret Key';
+$_['entry_test_publishable_key']		 = 'Test Publishable Key';
+$_['entry_test_secret_key']			 = 'Test Secret Key';
+$_['entry_merchant_id']	        		 = 'Merchant Id';
+$_['entry_payment_mode']			 = 'Payment Mode';
+$_['entry_payment_currency']			 = 'Payment Currency';
+$_['entry_invoice']				 = 'Merchant txn. ref.';
+$_['entry_sort_order']				 = 'Sort order';
+$_['entry_order_success_status']		 = 'Payment Success Status';
+$_['entry_order_declined_status']		 = 'Payment Declined Status';
+$_['entry_geo_zone']				 = 'Geo zone';
+$_['entry_cards']				 = 'Acceptable Credit Cards';
+$_['entry_status']				 = 'Status';
+
+
+// Help
+$_['help_invoice']				 = 'A reference for the transaction set by the merchant [without space] (ex: invoice number).';
+$_['help_order_success_status']			 = 'Set the order status when payment processed successfully.';
+$_['help_order_declined_status']		 = 'Set the order status when payment process declined or failed.';
+$_['help_payment_mode']				 = 'Set the payment mode Test and Live';
+$_['help_merchant_id']				 = 'Also known as Merchant Code, same can be found on your dashboard under account setting';
+
+// Tab
+$_['tab_account']				 = 'API Settings';
+
+
+// Error
+$_['error_secret_key']				 = 'Secret Key issued by Dasgateway is required';
+$_['error_invoice']				 = 'Merchant transaction reference (Optional)';
+$_['error_data_missing']			 = 'Data missing';
+$_['error_merchant_id']	        		 = 'Merchant Id is required';
---
+++ upload/catalog/language/en-gb/extension/payment/dasgatewayqr.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['text_title']			= '<img src="admin/view/image/payment/paymentoption.png" alt="Payment Option" title="SME Dasgateway" style="border: 1px solid #EEEEEE; width:100px;" />';
+
+// Button
+$_['button_confirm']			= 'Continue';
+
+// Text
+
+$_['text_card_number']			= 'Credit card number';
+$_['text_card_month']			= 'Expiry Month';
+$_['text_card_year']			= 'Expiry Year';
+$_['text_card_cvv']			= 'CVV';
+$_['text_card_name']			= 'Card Holder Name';
+
+// Error
+$_['error_failed']			= 'Unable to process your payment, please try again';
+$_['curl_error']			= 'Unable to process payment due to server issue. Kindly check with your Merchant(curl)';
